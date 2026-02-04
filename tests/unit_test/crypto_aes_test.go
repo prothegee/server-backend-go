c5f19e0fb498269d1e7536f09102125ffffa3275
prothegee <prothegee@proton.me>
2026-02-04 18:31:07 +0700

    formating line

--- tests/unit_test/crypto_aes_test.go
+++ tests/unit_test/crypto_aes_test.go
@@ -15,11 +15,13 @@ func Test_AES_CBC(t *testing.T) {
 	ivB := []byte(ivS)
 	ikB := []byte(ikS)
 
-	encrypted, err := pkg.AES_CBC_Encrypt(messageB, ivB, ikB); if err != nil {
+	encrypted, err := pkg.AES_CBC_Encrypt(messageB, ivB, ikB)
+	if err != nil {
 		t.Errorf("ERROR: %v\n", err)
 	}
 
-	decrypted, err := pkg.AES_CBC_Decrypt(encrypted, ivB, ikB); if err != nil {
+	decrypted, err := pkg.AES_CBC_Decrypt(encrypted, ivB, ikB)
+	if err != nil {
 		t.Errorf("ERROR: %v\n", err)
 	}
 
@@ -37,11 +39,13 @@ func Test_AES_GCM(t *testing.T) {
 	ivB := []byte(ivS)
 	ikB := []byte(ikS)
 
-	encrypted, err := pkg.AES_GCM_Encrypt(messageB, ivB, ikB); if err != nil {
+	encrypted, err := pkg.AES_GCM_Encrypt(messageB, ivB, ikB)
+	if err != nil {
 		t.Errorf("ERROR: %v\n", err)
 	}
 
-	decrypted, err := pkg.AES_GCM_Decrypt(encrypted, ivB, ikB); if err != nil {
+	decrypted, err := pkg.AES_GCM_Decrypt(encrypted, ivB, ikB)
+	if err != nil {
 		t.Errorf("ERROR: %v\n", err)
 	}
 
@@ -49,4 +53,3 @@ func Test_AES_GCM(t *testing.T) {
 		t.Errorf("ERROR: original message is not match with decrypted value\n")
 	}
 }
-
--- tests/unit_test/crypto_argon2_test.go
+++ tests/unit_test/crypto_argon2_test.go
@@ -8,16 +8,17 @@ import (
 
 func Test_Argon2id(t *testing.T) {
 	var err error
-	var password, salt, result string = "strong123!", "abcdefghijklmnop", "";
+	var password, salt, result string = "strong123!", "abcdefghijklmnop", ""
 
 	argon2Params := pkg.Argon2idParams_default
 
-	result, err = pkg.Argon2id(password, []byte(salt), argon2Params); if err != nil {
+	result, err = pkg.Argon2id(password, []byte(salt), argon2Params)
+	if err != nil {
 		t.Errorf("ERROR: %v\n", err)
 	}
 
-	_, err = pkg.Argon2idVerify(password, result); if err != nil {
+	_, err = pkg.Argon2idVerify(password, result)
+	if err != nil {
 		t.Errorf("ERROR: %v\n", err)
 	}
 }
-
