61182ac6afaf8c1a845014c11b8c0e9971831d08
prothegee <prothegee@proton.me>
2026-02-05 17:52:46 +0700

    change how test behave

--- run-test.sh
+++ run-test.sh
@@ -5,18 +5,16 @@ export CURRENT_DIR=$(pwd);
 export UNIT_TEST_DIR=$CURRENT_DIR/tests/unit_test;
 export BACKEND_API_TEST_DIR=$CURRENT_DIR/tests/backend_api;
 
-# assertion & data test
+# behavioural test first (they create the database!)
+cd $BACKEND_API_TEST_DIR;
+go test -v;
+
+echo "INFO: test in \"$BACKEND_API_TEST_DIR\" finished";
+
+# now unit tests should pass because database exists
 cd $UNIT_TEST_DIR;
 go test -v;
 
 echo "INFO: test in \"$UNIT_TEST_DIR\" finished";
 
-# behavioural test
-cd $BACKEND_API_TEST_DIR;
-go test -v;
-
-echo "INFO: test in \"$BACKEND_API_TEST_DIR\" finished";
-# echo "INFO: test in \"$BACKEND_API_TEST_DIR\" need to run manually after backend running";
-
 cd $CURRENT_DIR;
-
